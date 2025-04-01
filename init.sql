7c6d18ca6b4bdad0c401e3296bbb76039bda178b
Дмитрий Александрович <dimagolang7@gmail.com>
2025-04-01 13:11:41 +0300

    feat: feat db

--- init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS flights (
-                                       id SERIAL PRIMARY KEY,
-                                       destination_from TEXT NOT NULL,
-                                       destination_to TEXT NOT NULL,
-                                       deleted_at TIMESTAMP DEFAULT NULL
-);
